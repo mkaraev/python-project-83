f06a3c55ef1febf7f4feb87e91a64af35a729a9c
mkaraev <maruf.karaev97@gmail.com>
2022-12-18 00:31:41 +0500

    Add html templates for main page

--- page_analyzer/main.py
+++ page_analyzer/main.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/healthcheck")
 def hello_world():
+    return "App is running"
+
+
+@app.route("/")
+def index():
     return render_template("index.html")

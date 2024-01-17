b8b75dc690e5d17e300709009a87b9bd45e28ba8
ya-mkaraev <marufkaraev@yandex-team.ru>
2024-01-17 15:52:07 +0100

    Refactor database connection

--- page_analyzer/main.py
+++ page_analyzer/main.py
@@ -5,132 +5,112 @@ import psycopg2
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from flask import (
-    Flask,
-    abort,
-    flash,
-    redirect,
-    render_template,
-    request,
-    url_for,
-)
+from flask import (Flask, abort, flash, redirect, render_template, request,
+                   url_for)
 
-from page_analyzer import urls, db
+from page_analyzer import db, urls
 
 load_dotenv()
 
 app = Flask(__name__, template_folder="templates")
 
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "test-key")
+app.config["DATABASE_URL"] = os.getenv("DATABASE_URL")
 
 
-def get_db():
-    return psycopg2.connect(app.config['DATABASE_URL'])
-
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.get('/urls')
+@app.get("/urls")
 def urls_show():
-    conn = get_db()
-    urls = db.get_urls(conn)
-    checks = db.get_url_checks(conn)
-    conn.close()
+    urls = db.get_urls()
+    checks = db.get_url_checks()
 
     return render_template(
-        'urls/index.html',
+        "urls/index.html",
         data=zip_longest(urls, checks),
     )
 
 
-@app.post('/urls')
+@app.post("/urls")
 def post_url():
-    url = request.form['url']
+    url = request.form["url"]
     errors = urls.validate(url)
     if errors:
         for error in errors:
-            flash(error, 'danger')
-        return render_template('index.html', url_name=url), 422
+            flash(error, "danger")
+        return render_template("index.html", url_name=url), 422
 
-    conn = get_db()
     normalized_url = urls.normalize(url)
-    existed_url = db.get_url_by_name(conn, normalized_url)
+    existed_url = db.get_url_by_name(normalized_url)
 
     if existed_url:
         id = existed_url.id
-        flash('Страница уже существует', 'info')
+        flash("Страница уже существует", "info")
     else:
-        id = db.create_url(conn, normalized_url)
-        flash('Страница успешно добавлена', 'success')
+        id = db.create_url(normalized_url)
+        flash("Страница успешно добавлена", "success")
 
-    conn.close()
-
-    return redirect(url_for('url_show', id=id))
+    return redirect(url_for("url_show", id=id))
 
 
-@app.route('/urls/<int:id>')
+@app.route("/urls/<int:id>")
 def url_show(id):
-    conn = get_db()
-    url = db.get_url_by_id(conn, id)
+    url = db.get_url_by_id(id)
     if url is None:
         abort(404)
 
-    checks = db.get_checks_by_url_id(conn, id)
-    conn.close()
+    checks = db.get_checks_by_url_id(id)
 
     return render_template(
-        'urls/url.html',
+        "urls/url.html",
         url=url,
         checks=checks,
     )
 
 
 def parse_page(page):
-    soup = BeautifulSoup(page, 'html.parser')
-    title = soup.find('title').text if soup.find('title') else ''
-    h1 = soup.find('h1').text if soup.find('h1') else ''
-    description = soup.find('meta', attrs={'name': 'description'})
+    soup = BeautifulSoup(page, "html.parser")
+    title = soup.find("title").text if soup.find("title") else ""
+    h1 = soup.find("h1").text if soup.find("h1") else ""
+    description = soup.find("meta", attrs={"name": "description"})
     if description:
-        description = description['content']
+        description = description["content"]
     else:
-        description = ''
+        description = ""
     return {
-        'title': title[:255],
-        'h1': h1[:255],
-        'description': description[:255],
+        "title": title[:255],
+        "h1": h1[:255],
+        "description": description[:255],
     }
 
 
-@app.route('/urls/<int:id>/checks', methods=['POST'])
+@app.route("/urls/<int:id>/checks", methods=["POST"])
 def url_checks(id):
-    conn = get_db()
-    url = db.get_url_by_id(conn, id)
+    url = db.get_url_by_id(id)
     try:
         resp = requests.get(url.name)
         resp.raise_for_status()
     except requests.exceptions.RequestException:
-        flash('Произошла ошибка при проверке', 'danger')
-        return redirect(url_for('url_show', id=id))
+        flash("Произошла ошибка при проверке", "danger")
+        return redirect(url_for("url_show", id=id))
 
     page = resp.text
-    check_data = {'id': id, 'status_code': resp.status_code, **parse_page(page)}
+    check_data = {"id": id, "status_code": resp.status_code, **parse_page(page)}
 
-    db.create_url_check(conn, check_data)
-    flash('Страница успешно проверена', 'success')
-    conn.close()
+    db.create_url_check(check_data)
+    flash("Страница успешно проверена", "success")
 
-    return redirect(url_for('url_show', id=id))
+    return redirect(url_for("url_show", id=id))
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('errors/404.html'), 404
+    return render_template("errors/404.html"), 404
 
 
 @app.errorhandler(500)
 def server_error(error):
-    return render_template('errors/500.html'), 500
+    return render_template("errors/500.html"), 500
